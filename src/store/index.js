d671792b635dd6a10c6d2263eb8daa849068c4ff
Avinash <avinashkmg1310@gmail.com>
2024-09-17 06:35:57 +0530

    setting up @redux-toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,45 +1,71 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+//createSlice makes us prepare a slice of our global state.
+//When we have different pieces of state that are not directly related.
 
 const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initialState, action) => {
-	if (action.type === "increment") {
-		return { counter: state.counter + 1, showCounter: state.showCounter };
-	}
+const counterSlice = createSlice({
+	name: "counter",
+	initialState,
+	reducers: {
+		increment(state) {
+			state.counter++;
+		},
+		decrement(state) {
+			state.counter--;
+		},
+		incrementBy(state, action) {
+			state.counter += action.payload;
+		},
+		decrementBy(state, action) {
+			state.counter -= action.payload;
+		},
+		toggleCounter(state) {
+			state.showCounter = !state.showCounter;
+		},
+	},
+});
 
-	if (action.type === "decrement") {
-		return { counter: state.counter - 1, showCounter: state.showCounter };
-	}
-	if (action.type === "INCREMENTBY") {
-		return {
-			counter: state.counter + action.payload,
-			showCounter: state.showCounter,
-		};
-	}
-	if (action.type === "DECREMENTBY") {
-		return {
-			counter: state.counter - action.payload,
-			showCounter: state.showCounter,
-		};
-	}
-
-	if (action.type === "toggle") {
-		return {
-			showCounter: !state.showCounter,
-			counter: state.counter,
-		};
-	}
-
-	return state;
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({
+	reducer: counterSlice.reducer,
+});
 
 export default store;
-//Exporting so that the React components can use Redux store
 
-// const counterSuscriber = () => {
-// 	const latestState = store.getState();
+// How to Basic Redux. //
+// const counterReducer = (state = initialState, action) => {
+// 	if (action.type === "increment") {
+// 		return { counter: state.counter + 1, showCounter: state.showCounter };
+// 	}
+
+// 	if (action.type === "decrement") {
+// 		return { counter: state.counter - 1, showCounter: state.showCounter };
+// 	}
+// 	if (action.type === "INCREMENTBY") {
+// 		return {
+// 			counter: state.counter + action.payload,
+// 			showCounter: state.showCounter,
+// 		};
+// 	}
+// 	if (action.type === "DECREMENTBY") {
+// 		return {
+// 			counter: state.counter - action.payload,
+// 			showCounter: state.showCounter,
+// 		};
+// 	}
+
+// 	if (action.type === "toggle") {
+// 		return {
+// 			showCounter: !state.showCounter,
+// 			counter: state.counter,
+// 		};
+// 	}
+
+// 	return state;
 // };
+// const store = createStore(counterReducer);
 
-// store.subscribe(counterSuscriber);
+// export default store;
+//Exporting so that the React components can use Redux store
